255403055cb3aa5b3706c8ac98955569caa6c28c
Mina-T <taleblou.mina@gmail.com>
2025-12-22 13:25:55 +0100

    atom_label

--- utils/labels.py
+++ utils/labels.py
@@ -1,8 +1,20 @@
 def get_drautz_label(key):
-    categories = ['cluster', 'sp2', 'sp3', 'bulk', 'amorph']
-    for cat in categories:
+    '''
+    given a name, returns Drautz category and category-id
+    '''
+    categories_dict = {'cluster':1, 'sp2':2, 'sp3':3, 'bulk':4, 'amorph':5}
+    for cat in categories_dict.keys():
         if cat in key:
-            return cat
-        else:
-            continue
+            return cat, categories_dict[cat]
+    
+    return None, None 
+
+
+def drautz_atom_label(cat, sys_id, atom_id):
+    '''
+    labels each atom of Drautz test set. 
+    '''
+    cat_id = get_drautz_label(cat)
+    atom_label = cat_id + '_' + sys_id + '_' + atom_id
+    return atom_label
 
