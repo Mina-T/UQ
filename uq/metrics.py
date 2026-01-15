47f7710239ed37f015f9dcece7ee21b841d07f23
Mina Taleblou <57327054+Mina-T@users.noreply.github.com>
2026-01-15 13:17:09 +0100

    measure_correlation class

--- uq/metrics.py
+++ uq/metrics.py
@@ -2,24 +2,29 @@ import numpy as np
 from scipy.stats import pearsonr, spearmanr
 from sklearn.metrics import r2_score
 
-def nllf(model_err, model_std_dev, eps=1e-12):
-    model_err = np.linalg.norm(np.asarray(model_err))
-    model_std_dev = np.linalg.norm(np.asarray(model_std_dev))
-    var = model_std_dev**2
-    normalized_err = (model_err)**2 / var
-    sm = (np.log(2* np.pi*var)  + normalized_err).mean()
-    nll = 0.5*sm
-    return nll
+class measure_correlation:
+    def __init__(self, uq_metric, ens_uncertainty):
+        self.uq_metric = uq_metric
+        self.ens_uncertainty = ens_uncertainty
 
-def R2(y_ref, y_pred):
-    return r2_score(y_ref, y_pred)
+    def nllf(self, eps=1e-12):
+        uq_metric = np.linalg.norm(np.asarray(self.uq_metric))
+        ens_uncertainty = np.linalg.norm(np.asarray(self.ens_uncertainty))
+        var = ens_uncertainty**2
+        normalized_err = (uq_metric)**2 / var
+        sm = (np.log(2* np.pi*var)  + normalized_err).mean()
+        nll = 0.5*sm
+        return nll
+
+    def R2(self):
+        return r2_score(self.uq_metric, self.ens_uncertainty)
 
 
-def pearson(x, y):
-    pearson_corr, p = pearsonr(x, y)
-    return pearson_corr
+    def pearson(self):
+        pearson_corr, p = pearsonr(self.uq_metric, self.ens_uncertainty)
+        return pearson_corr
 
 
-def spearsman(x, y):
-    spearman_corr, p = spearmanr(x, y)
+    def spearsman(self):
+        spearman_corr, p = spearmanr(self.uq_metric, self.ens_uncertainty)
     return spearman_corr
